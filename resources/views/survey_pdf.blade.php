7b01de82cc814fda5175acf198c34fd0ba865a7a
osokarp <bayu230498@gmail.com>
2021-03-14 18:24:50 +0700

    revisi tampilan pdf survey

--- resources/views/survey_pdf.blade.php
+++ resources/views/survey_pdf.blade.php
@@ -12,22 +12,49 @@
         }
     </style>
     <center>
-        <h4>ACCESS REQUEST FORM</h4>
-        <h4>Nomor : ARF/001/DCDV/XI/2019</h4>
+        <h5>ACCESS REQUEST FORM</h5>
+        <h5>Nomor : ARF/001/DCDV/XI/2019</h5>
     </center>
 
     <table class='table table-bordered'>
-        <tr><td><b>Time of Request : </b>{{$survey->created_at}}</td></tr>
-        <tr><td><b>No. </b>{{$survey->survey_id}}</td></tr>
-        <tr><td><b>Purpose of Work : </b>{{$survey->purpose_work}}</td></tr>
-        <tr><td><b>Visitor Name : </b>{{$survey->visitor_name}}</td></tr>
+        <tr><td>Time of Request : {{$survey->created_at}}</td></tr>
+        <tr><td>Purpose of Work : {{$survey->purpose_work}}</td></tr>
+    </table>
+
+    <table class='table table-bordered'>
+        <tr>
+            <td colspan="2"><b>Requestor Bali Tower</b></td>
+        </tr>
+        <tr>
+            <td>Name :</td>
+            <td>Phone Number : </td>
+        </tr>
+        <tr>
+            <td colspan="2">Department :</td>
+    </table>
+
+    <table class='table table-bordered'>
+        <tr>
+            <td colspan="2"><b>Visitor</b></td>
+        </tr>
+        <tr>
+            <td>Name : {{$survey->visitor_name}}</td>
+            <td>ID : {{$survey->visitor_id}}</td>
+        </tr>
+        <tr>
+            <td>Company : {{$survey->visitor_company}}</td>
+            <td>Phone Number : {{$survey->visitor_phone}}</td>
+        </tr>
+        <tr>
+            <td colspan="2">Department : {{$survey->visitor_department}}</td>
+        </tr>
     </table>
 
         <table class='table table-bordered'>
             <thead>
                 <tr>
-                    <th>Review</th>
-                    <th>Check</th>
+                    <th>Reviewed By</th>
+                    <th>Checked By</th>
                     <th>Security</th>
                     <th>Dept.Head</th>
                 </tr>
