8573cae9a2c73cbb917d6145d08d80d120681ea3
osokarp <bayu230498@gmail.com>
2021-03-19 17:34:59 +0700

    tambahan table di users

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,6 +52,8 @@
                             </div>
                         </div>
 
+
+
                         <div class="form-group row">
                             <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
 
