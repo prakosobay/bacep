ba8060f45a42538d8e64efdce30ed6c78b437040
osokarp <bayu230498@gmail.com>
2021-04-19 02:55:01 +0700

    tampilan notif email

--- resources/views/emailku.blade.php
+++ resources/views/emailku.blade.php
@@ -5,33 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Email Notify</title>
-    <style>
-        body {
-            background-color:#bdc3c7;
-            margin:0;
-        }
-        .card {
-            background-color:#fff;
-            padding:20px;
-            margin:20%;
-            text-align:center;
-            margin:0px auto;
-            width: 580px;
-            max-width: 580px;
-            margin-top:10%;
-            box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
-        }
 
-        .garis {
-            width: 75%;
-        }
-
-    </style>
 </head>
 <body>
     <div class="card">
         <h3 class="">Hallo, {{ $nama }} !</h3>
-        <hr class="garis">
         <p>{{ $pesan }}</p>
 
         <p>Silahkan Login di  <a href="http://localhost:8000/approval/all">http://localhost:8000/approval/all</a></p>
