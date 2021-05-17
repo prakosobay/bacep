2825e5b0766bf8dd3d7b4da82dbdca4e9ef9ecf2
prakosobay <prakosobayu98@gmail.com>
2021-05-17 21:23:11 +0700

    email dan ip dah fiix

--- app/Http/Controllers/CleaningController.php
+++ app/Http/Controllers/CleaningController.php
@@ -23,7 +23,7 @@ class CleaningController extends Controller
         if (Auth::user()->role == 'bm')
             $cleaning = Cleaning::create($request->all());
 
-        foreach (['darajat.indraputra@balitower.co.id'] as $recipient) {
+        foreach (['rio.christian@balitower.co.id'] as $recipient) {
             Mail::to($recipient)->send(new NotifEmail());
         }
 
@@ -77,17 +77,17 @@ class CleaningController extends Controller
 
         $role_to = '';
         if (($lasthistoryC->role_to == 'review')) {
-            foreach (['bayu.prakoso@balitower.co.id'] as $recipient) {
+            foreach (['rafli.ashshiddiqi@balitower.co.id'] as $recipient) {
                 Mail::to($recipient)->send(new NotifEmail());
             }
             $role_to = 'check';
         } elseif (($lasthistoryC->role_to == 'check')) {
-            foreach (['bayu230498@gmail.com'] as $recipient) {
+            foreach (['bayu.prakoso@balitower.co.id'] as $recipient) {
                 Mail::to($recipient)->send(new NotifEmail());
             }
             $role_to = 'security';
         } elseif (($lasthistoryC->role_to == 'security')) {
-            foreach (['rio.christian@balitower.co.id'] as $recipient) {
+            foreach (['panggah@balitower.co.id'] as $recipient) {
                 Mail::to($recipient)->send(new NotifEmail());
             }
             $role_to = 'boss';
@@ -102,7 +102,7 @@ class CleaningController extends Controller
         ]);
 
         if ($lasthistoryC->role_to == 'boss') {
-            foreach (['prakosobayu98@gmail.com'] as $recipient) {
+            foreach (['dc@balitower.co.id'] as $recipient) {
                 Mail::to($recipient)->send(new NotifFull());
             }
             $cleaning = Cleaning::where('cleaning_id', $request->cleaning_id)->first();
--- resources/views/emailku.blade.php
+++ resources/views/emailku.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Email Notify</title>
+    <title>Email Notif Permit</title>
 
 </head>
 <body>
@@ -12,7 +12,7 @@
         <h3 class="">Hallo, DC Team!</h3>
         <p>Mohon untuk review dan check permit dari team BM yang telah di submit.
             Link terlampir </p>
-        <p>Silahkan Login di  <a href="http://172.16.45.239:8000/approval/all">http://172.16.45.239:8000/approval/all</a></p>
+        <p>Silahkan Login di  <a href="http://172.16.45.195:8000/approval/all">http://172.16.45.195:8000/approval/all</a></p>
 
     </div>
 </body>
