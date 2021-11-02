979a8dfe8c3a7205a1c2d146691acf698bf74504
prakosobay <prakosobayu98@gmail.com>
2021-11-02 14:52:33 +0700

    show table checklist done

--- resources/views/checklist/show.blade.php
+++ resources/views/checklist/show.blade.php
@@ -14,60 +14,273 @@
             </a>
         </div>
         <div class="card-body">
-            <div class="row">
-                <div class="col-sm">
-                    <label class="text-center"><strong>Generator 1</strong></label>
-                    <div class="form-group row">
-                        <label for="tanggal1" class="col-sm-2 col-form-label"><strong>Tanggal :</strong></label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="tanggal1" value="{{Carbon\Carbon::parse($show->date1)->format('d-m-Y')}}" readonly>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="time1" class="col-sm-2 col-form-label"><strong>Waktu :</strong></label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="time1" value="{{Carbon\Carbon::parse($show->time1)->format('d-m-Y')}}" readonly>
-                        </div>
-                    </div>
-
-                    <label for="tighten" class="col-sm-10 col-form-label"><strong>1. Tighten Battery Connection :</strong></label>
-                    <div class="form-group row">
-                        @if($show->input1 == '0')
-                        <input type="text" class="form-control col-sm-3 ml-3" id="tighten" value="NOT OK" readonly>
-                        @elseif($show->input1 == '1')
-                        <input type="text" class="form-control col-sm-3 ml-3" id="tighten" value="OK" readonly>
-                        @endif
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" id="tighten" value="{{$show->remark1}}" readonly>
-                        </div>
-                    </div>
-
-                    <label for="tighten" class="col-sm-10 col-form-label"><strong>2. Measurement Battery Voltage (Open Circuit) :</strong></label>
-                    <div class="form-group row">
-                        @if($show->input2 == '0')
-                        <input type="text" class="form-control col-sm-3 ml-3" id="tighten" value="NOT OK" readonly>
-                        @elseif($show->input2 == '1')
-                        <input type="text" class="form-control col-sm-3 ml-3" id="tighten" value="OK" readonly>
-                        @endif
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" id="tighten" value="{{$show->remark2}}" readonly>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm">
-                    <label class="text-center"><strong>Generator 2</strong></label>
-                    {{-- <div class="table-responsive">
-                        <div class="form-group">
-
-                            <label for="stok"><strong>Stock Saat Ini</strong></label>
-                            <input type="number" class="form-control" id="stok" value="" readonly>
-
-                            <label for="jumlah"><strong>Jumlah</strong></label>
-                            <input type="number" class="form-control @error('jumlah') is-invalid @enderror" required autocomplete="jumlah" id="jumlah" name="jumlah" autofocus>
-
-                        </div>
-                    </div> --}}
-                </div>
+            <div class="table-responsive">
+                <h3 class="h3 mb-2 text-black-1000 text-center"><strong>Genset 1</strong></h3>
+                <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead class="text-center">
+                        <tr>
+                            <th><strong>No. </strong></th>
+                            <th><strong>Task Detail</strong></th>
+                            <th><strong>Unit Status</strong></th>
+                            <th><strong>Remarks</strong></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1. </td>
+                            <td>Tighten Battery Connection</td>
+                            @if($show->input1 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input1 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark1}}</td>
+                        </tr>
+                        <tr>
+                            <td>2. </td>
+                            <td>Measurement Battery Voltage (Open Circuit)</td>
+                            @if($show->input2 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input2 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark2}}</td>
+                        </tr>
+                        <tr>
+                            <td>3. </td>
+                            <td>Measurement Battery Voltage (Charging Condition)</td>
+                            @if($show->input3 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input3 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark3}}</td>
+                        </tr>
+                        <tr>
+                            <td>4. </td>
+                            <td>Check For Lubricant Oil Level</td>
+                            @if($show->input4 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input4 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark4}}</td>
+                        </tr>
+                        <tr>
+                            <td>5. </td>
+                            <td>Status Storage Tank Level</td>
+                            @if($show->input5 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input5 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark5}}</td>
+                        </tr>
+                        <tr>
+                            <td>6. </td>
+                            <td>Status Daily Tank Level</td>
+                            @if($show->input6 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input6 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark6}}</td>
+                        </tr>
+                        <tr>
+                            <td>7. </td>
+                            <td>Measurement Engine Running (hours)</td>
+                            @if($show->input7 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input7 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark7}}</td>
+                        </tr>
+                        <tr>
+                            <td>8. </td>
+                            <td>Genset Mode</td>
+                            @if($show->input8 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input8 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark8}}</td>
+                        </tr>
+                        <tr>
+                            <td>9. </td>
+                            <td>Switch Selector Panel Coupler</td>
+                            @if($show->input9 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input9 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark9}}</td>
+                        </tr>
+                        <tr>
+                            <td>10. </td>
+                            <td>Switch Selector Panel PKG</td>
+                            @if($show->input10 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input10 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark10}}</td>
+                        </tr>
+                        <tr>
+                            <td>11. </td>
+                            <td>Switch Selector PUTR 1</td>
+                            @if($show->input11 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input11 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark11}}</td>
+                        </tr>
+                        <tr>
+                            <td>12. </td>
+                            <td>Switch Selector PUTR 2</td>
+                            @if($show->input12 == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input12 == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark12}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <h3 class="h3 mb-2 text-black-1000 text-center"><strong>Genset 2</strong></h3>
+                <table class="table table-bordered" width="100%" cellspacing="0">
+                    <thead class="text-center">
+                        <tr>
+                            <th><strong>No. </strong></th>
+                            <th><strong>Task Detail</strong></th>
+                            <th><strong>Unit Status</strong></th>
+                            <th><strong>Remarks</strong></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1. </td>
+                            <td>Tighten Battery Connection</td>
+                            @if($show->input1a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input1a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark1a}}</td>
+                        </tr>
+                        <tr>
+                            <td>2. </td>
+                            <td>Measurement Battery Voltage (Open Circuit)</td>
+                            @if($show->input2a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input2a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark2a}}</td>
+                        </tr>
+                        <tr>
+                            <td>3. </td>
+                            <td>Measurement Battery Voltage (Charging Condition)</td>
+                            @if($show->input3a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input3a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark3a}}</td>
+                        </tr>
+                        <tr>
+                            <td>4. </td>
+                            <td>Check For Lubricant Oil Level</td>
+                            @if($show->input4a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input4a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark4a}}</td>
+                        </tr>
+                        <tr>
+                            <td>5. </td>
+                            <td>Status Storage Tank Level</td>
+                            @if($show->input5a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input5a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark5a}}</td>
+                        </tr>
+                        <tr>
+                            <td>6. </td>
+                            <td>Status Daily Tank Level</td>
+                            @if($show->input6a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input6a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark6a}}</td>
+                        </tr>
+                        <tr>
+                            <td>7. </td>
+                            <td>Measurement Engine Running (hours)</td>
+                            @if($show->input7a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input7a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark7a}}</td>
+                        </tr>
+                        <tr>
+                            <td>8. </td>
+                            <td>Genset Mode</td>
+                            @if($show->input8a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input8a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark8a}}</td>
+                        </tr>
+                        <tr>
+                            <td>9. </td>
+                            <td>Switch Selector Panel Coupler</td>
+                            @if($show->input9a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input9a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark9a}}</td>
+                        </tr>
+                        <tr>
+                            <td>10. </td>
+                            <td>Switch Selector Panel PKG</td>
+                            @if($show->input10a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input10a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark10a}}</td>
+                        </tr>
+                        <tr>
+                            <td>11. </td>
+                            <td>Switch Selector PUTR 1</td>
+                            @if($show->input11a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input11a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark11a}}</td>
+                        </tr>
+                        <tr>
+                            <td>12. </td>
+                            <td>Switch Selector PUTR 2</td>
+                            @if($show->input12a == '0')
+                            <td class="text-center">NOT OK</td>
+                            @elseif($show->input12a == '1')
+                            <td class="text-center">OK</td>
+                            @endif
+                            <td class="text-center">{{$show->remark12a}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
