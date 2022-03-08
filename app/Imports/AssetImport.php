c137ac0da42f416d8fb334c782c4aa8b9ebb5929
prakosobay <prakosobay@gmail.com>
2022-03-08 10:19:05 +0700

    export barang digunakan

--- app/Imports/AssetImport.php
+++ app/Imports/AssetImport.php
@@ -2,7 +2,7 @@
 
 namespace App\Imports;
 
-use App\Models\{Asset, AssetKeluar, AssetMasuk};
+use App\Models\{Asset, AssetKeluar, AssetMasuk, AssetUse};
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\{ToCollection, WithStartRow};
 
@@ -14,24 +14,26 @@ class AssetImport implements ToCollection, WithStartRow
      * @return \Illuminate\Database\Eloquent\Model|null
      */
 
-    // private $assets;
-    // public function __construct()
-    // {
-    //     $this->asset = Asset::select('id')->get();
-    // }
+    private $assets;
+    public function __construct()
+    {
+        $this->asset = Asset::select('id')->get();
+    }
     // public function collection(Collection $rows1)
     // {
     //     // echo '<pre>';
     //     // var_dump($rows1);
     //     // die;
+    //     Asset::truncate();
     //     foreach ($rows1 as $row) {
     //         Asset::create([
     //             'nama_barang' => $row[1],
     //             'jumlah' => $row[5],
-    //             'satuan' => $row[6],
-    //             // 'kondisi' => $row[7],
-    //             'note' => $row[8],
-    //             'lokasi' => $row[9],
+    //             'digunakan' => $row[6],
+    //             'sisa' => $row[7],
+    //             'satuan' => $row[8],
+    //             'note' => $row[10],
+    //             'lokasi' => $row[11],
     //         ]);
     //     }
     // }
@@ -41,6 +43,7 @@ class AssetImport implements ToCollection, WithStartRow
     //     // echo '<pre>';
     //     // var_dump($rows3);
     //     // die;
+    //     AssetMasuk::truncate();
     //     foreach ($rows3 as $row) {
     //         AssetMasuk::create([
     //             'tanggal' => $row[0],
@@ -53,13 +56,32 @@ class AssetImport implements ToCollection, WithStartRow
     //     }
     // }
 
-    public function collection(Collection $rows2)
+    // public function collection(Collection $rows2)
+    // {
+    //     // echo '<pre>';
+    //     // var_dump($rows2);
+    //     // die;
+    //     AssetKeluar::truncate();
+    //     foreach ($rows2 as $row) {
+    //         AssetKeluar::create([
+    //             'tanggal' => $row[0],
+    //             'asset_id' => $row[1],
+    //             'nama_barang' => $row[2],
+    //             'jumlah' => $row[3],
+    //             'ket' => $row[4],
+    //             'pencatat' => $row[5],
+    //         ]);
+    //     }
+    // }
+
+    public function collection(Collection $rows5)
     {
         // echo '<pre>';
-        // var_dump($rows2);
+        // var_dump($rows5);
         // die;
-        foreach ($rows2 as $row) {
-            AssetKeluar::create([
+        AssetUse::truncate();
+        foreach ($rows5 as $row) {
+            AssetUse::create([
                 'tanggal' => $row[0],
                 'asset_id' => $row[1],
                 'nama_barang' => $row[2],
