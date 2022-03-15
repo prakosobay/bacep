4b409afbb3d22e5563912f02fbf7324d38614db4
prakosobay <prakosobay@gmail.com>
2022-03-15 17:26:20 +0700

    login2

--- resources/views/auth/login2.blade.php
+++ resources/views/auth/login2.blade.php
@@ -6,33 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <title>PT Bali Towerindo Sentra Tbk</title>
 
+    <link href="{{asset('css/login.css')}}">
+
     <style>
-        .images{
-            position: absolute;
-            width: 100%;
-            height: 100%;
-            left: 0px;
-            right: 0px;
+        .border{
+            border: solid;
+
         }
-    </style>
+        </style>
 </head>
 <body>
-<div class="container-fluid">
-    <div class="row-justify-content-center">
-        <div class="col-6">
-            <div class="card">
-                <img class="images" src="{{asset('gambar/login.png')}}" alt="">
-            </div>
-        </div>
+<div class="container-fluid border">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ipsum non quidem vel officia illo cumque commodi eaque explicabo ad quae perspiciatis tempore, blanditiis harum, natus reprehenderit quaerat aperiam asperiores rem. Quisquam repellat eligendi rerum natus eos facilis, officiis deleniti molestiae, nobis illum doloribus provident non cupiditate. Necessitatibus natus officia ab perspiciatis quae consequatur, repellendus obcaecati inventore, laborum reprehenderit dolorum. Reiciendis harum, reprehenderit delectus numquam amet saepe ab sequi nesciunt sapiente quidem enim totam magnam sed ipsam odit error tenetur! Ullam possimus et cumque non sed accusamus nihil, debitis voluptatem aliquam vel, rem natus facere recusandae beatae nisi omnis id.
 
-        <div class="col-6">
-
-        </div>
-    </div>
 </div>
 </body>
 </html>
