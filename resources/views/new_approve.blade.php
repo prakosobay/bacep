a69c1945968e9d33eb1cb917f6de8f059b0c1979
yogaagusr <99043676+yogaagusr@users.noreply.github.com>
2022-03-26 08:09:00 +0700

    otw navbar

--- resources/views/new_approve.blade.php
+++ resources/views/new_approve.blade.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Approval Permit</title>
-
+    <!-- Load an icon library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
     <link rel="stylesheet" href="{{asset('css/new_approve.css')}}" type="text/css">
+    
 </head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark py-0 my-0 navbar-bg">
+<div class="body">
+    <nav class="navbar navbar-expand-lg navbar-dark py-0 my-0 navbar-bg" position="fixed">
         <div class="container">
             <a class="navbar-brand" href="#">
                 <img src="{{asset('gambar/approval/logo_approve.png')}}" alt="" style="width: 170px; height:70px" class="img-fluid">
@@ -54,6 +55,21 @@
             </div>
         </div>
     </nav>
+    
+    <!-- The sidebar -->
+    <div class="sidebar">
+        <a href="#home"><i class="fa fa-star-o"></i>Approval Permit</a>
+        <a href="#services"><i class="fa fa-weixin"></i>Customer Colo</a>
+        <a href="#clients"><i class="fa fa-fw fa-user"></i>Permit Internal</a>
+        <a href="#contact"><i class="fa fa-suitcase"></i>Customer Survey</a>
+        <a href="#contact"><i class="fa fa-address-card"></i>Permit Vendor </a>
+        <a href="#contact"><i class="fa fa-user-secret"></i>Permit BM</a>
+        <a href="#contact"><i class="fa fa-users"></i>Permit Guest</a>
+    </div>
+
+    <div class="content">
+        <h1>HOME</h1>
+    </div>
 
     {{-- YANG INI JANGAN DU HAPUS !!! --}}
     <main>
@@ -62,5 +78,5 @@
     {{-- !!!!!!!!!!!! --}}
 
 
-</body>
+</div>
 </html>
