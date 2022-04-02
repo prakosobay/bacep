9b96bdf2f5ba4193410f285d2cc58c77f75002a0
yogaagusr <99043676+yogaagusr@users.noreply.github.com>
2022-04-02 07:51:22 +0700

    sidebar belum kelar

--- resources/views/new_approve.blade.php
+++ resources/views/new_approve.blade.php
@@ -12,9 +12,27 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link href="{{asset('vendor2/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('vendor2/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
+    <!-- Custom styles for this template -->
+    <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Custom styles for this page -->
+    <link href="{{asset('vendor2/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+
+    <style>
+        .margin-bottom{
+        margin-bottom: 10px !important;}
+    </style>
 </head>
 <body id="body-pd">
+  <div class="semua">
     <nav class="navbar navbar-expand-lg navbar-dark py-0 my-0 navbar-bg" >
         <div class="container">
             <a class="navbar-brand" href="#">
@@ -62,105 +80,170 @@
         @yield('content')
     </main>
     {{-- !!!!!!!!!!!! --}}
+<body id="page-top">
+    @include('sweetalert::alert')
 
-      <div class="sidebar close">
-        <ul class="nav-links">
-          <li>
-            <a href="#">
-              <i class='bx bx-grid-alt' ></i>
-              <span class="link_name">Approval Permit</span>
-            </a>
-            <ul class="sub-menu blank">
-              <li><a class="link_name" href="#">Approval Permit</a></li>
-            </ul>
-          </li>
-          <li>
-              <a href="#">
-                <i class='bx bx-collection' ></i>
-                <span class="link_name">Customer Colo</span>
-              </a>
-            <ul class="sub-menu blank">
-              <li><a class="link_name" href="#">Customer Colo</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i class='bx bx-pie-chart-alt-2' ></i>
-              <span class="link_name">Permit Internal</span>
-            </a>
-            <ul class="sub-menu blank">
-              <li><a class="link_name" href="#">Permit Internal</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i class='bx bx-line-chart' ></i>
-              <span class="link_name">Customer Survey</span>
-            </a>
-            <ul class="sub-menu blank">
-              <li><a class="link_name" href="#">Customer Survey</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i class='bx bx-compass' ></i>
-              <span class="link_name">Permit Vendor</span>
-            </a>
-            <ul class="sub-menu blank">
-              <li><a class="link_name" href="#">Permit Vendor</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i class='bx bxs-face'></i>
-              <span class="link_name">Permit BM</span>
-            </a>
-            <ul class="sub-menu blank">
-              <li><a class="link_name" href="#">Permit BM</a></li>
-            </ul>
-          </li>
-          <li>
-            <a href="#">
-              <i class='bx bx-plug' ></i>
-              <span class="link_name">Permit Guest</span>
-            </a>
-            <ul class="sub-menu blank">
-              <li><a class="link_name" href="#">Permit Gues</a></li>
-            </ul>
-        </li>
-   
-        <li>
-            <section class="home-section">
-              <li class="home-content">
-                  <i class='bx bx-menu' ></i>
-                <a href="#">
-                    <span class="link_name">Sidebar</span>
-                  </a>
-              </li>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
 
-            </section>
-          </li>
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    </ul>
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Home</span></a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="">
+                  <i class="fas fa-fw fa-tachometer-alt"></i>
+                  <span>1</span></a>
+           </li>
+
+           <li class="nav-item">
+            <a class="nav-link" href="">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>2</span></a>
+         </li>
+         <li class="nav-item">
+          <a class="nav-link" href="">
+              <i class="fas fa-fw fa-tachometer-alt"></i>
+              <span>3</span></a>
+       </li>
+       <li class="nav-item">
+        <a class="nav-link" href="">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>4</span></a>
+     </li>
+     <li class="nav-item">
+      <a class="nav-link" href="">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>5</span></a>
+   </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Nav Item - Tables -->
+            @can('isAdmin')
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Users</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Option :</h6>
+                        <a class="collapse-item" href="">Data User</a>
+                        <a class="collapse-item" href="">Data Role</a>
+                        <a class="collapse-item" href="">Relasi</a>
+                        {{-- <a class="collapse-item" href="{{url('a.update')}}">Update Barang</a> --}}
+                    </div>
+                </div>
+            </li>
+            @else
+
+            <!-- Divider -->
+            <hr class="sidebar-divider">
+
+            <!-- Checklist -->
+            <div class="sidebar-heading">
+                Checklist
+            </div>
+
+            <!-- Checklist Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                    aria-expanded="true" aria-controls="collapsePages">
+                    <i class="fas fa-fw fa-folder"></i>
+                    <span>Checklist Generator</span>
+                </a>
+                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Option:</h6>
+                        <a class="collapse-item" href="">Checklist Genset</a>
+                        <a class="collapse-item" href="">Table Checklist</a>
+                    </div>
+                </div>
+            </li>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+            @endcan
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+               
+                <main>
+                    @yield('content')
+                </main>
+                <!-- End of Topbar -->
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Manajemen Barang 2020</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
     </div>
-      
+    <!-- End of Page Wrapper -->
 
-<script>
-      let arrow = document.querySelectorAll(".arrow");
-      for (var i = 0; i < arrow.length; i++) {
-        arrow[i].addEventListener("click", (e)=>{
-       let arrowParent = e.target.parentElement.parentElement;
-       arrowParent.classList.toggle("showMenu");
-        });
-      }
-      let sidebar = document.querySelector(".sidebar");
-      let sidebarBtn = document.querySelector(".bx-menu");
-      sidebarBtn.addEventListener("click", ()=>{
-        sidebar.classList.toggle("close");
-      });
-      </script>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
 
- </body>
-    
+    <!-- Logout Modal-->
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('vendor2/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('vendor2/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('vendor2/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
+
+    {{-- SweetAlert2 --}}
+
+    {{-- <script src="sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="sweetalert2.min.css"> --}}
+
+    <!-- Page level plugins -->
+    <script src="{{asset('vendor2/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('vendor2/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{asset('js/demo/datatables-demo.js')}}"></script>
+
+</body>
+
 </html>
-
