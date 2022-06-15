39893a4628af83629045af1a1e51d2945ce80b50
yogaagusr <yogaagusr@gmail.com>
2022-06-15 18:43:36 +0700

    revisi max penambahan baris table jadi 5

--- resources/views/other/troubleshoot_form.blade.php
+++ resources/views/other/troubleshoot_form.blade.php
@@ -315,7 +315,7 @@
 {{--add table--}}
 <script>
 	$(document).ready(function() {
-    var max_fields      = 10; //maximum input boxes allowed
+    var max_fields      = 5; //maximum input boxes allowed
     var wrapper         = $("#input_fields_wrap"); //Fields wrapper
     var add_button      = $(".add_field_button"); //Add button ID
    
